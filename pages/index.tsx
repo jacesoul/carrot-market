d6c35ce8da79644e8d3c8d10b80151af21edcb57
jacesoul <seolju90@gmail.com>
2022-07-24 19:37:47 +0900

    Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,9 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className=" font-semibold">$99</span>
         </div>
-        <div className=" mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto">
+        <button className=" mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
           Checkout
-        </div>
+        </button>
       </div>
       <div className=" bg-white rounded-3xl shadow-xl overflow-hidden">
         <div className=" bg-blue-500 p-6 pb-14">
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
               <span className=" text-sm text-gray-500">Orders</span>
               <span className=" font-medium">340</span>
             </div>
-            <div className=" h-24 w-24 bg-red-400 rounded-full"></div>
+            <div className=" h-24 w-24 bg-teal-400 rounded-full"></div>
             <div className=" flex flex-col items-center">
               <span className=" text-sm text-gray-500">Spent</span>
               <span className=" font-medium">$340</span>
@@ -56,10 +56,10 @@ const Home: NextPage = () => {
           <span className=" font-medium text-xl">Swoon Lounge</span>
           <span className=" text-xs text-gray-500">Chair</span>
           <div className=" mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className=" space-x-2">
+              <button className=" w-5 h-5 rounded-full bg-yellow-500"></button>
+              <button className=" w-5 h-5 rounded-full bg-indigo-500"></button>
+              <button className=" w-5 h-5 rounded-full bg-teal-500"></button>
             </div>
             <div className=" flex items-center space-x-3">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
