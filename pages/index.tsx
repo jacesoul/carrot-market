37b6030803836a390a2eec14e65895dc11b1c3f5
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-08-01 22:42:41 +0900

    Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,30 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className=" bg-slate-400 py-20 px-20 xl:place-content-center grid lg:grid-cols-2 xl:grid-cols-3 gap-10 mx-auto">
-      <div className=" bg-white p-6 rounded-3xl shadow-xl flex flex-col justify-between">
-        <span className=" font-semibold text-3xl">Select Item</span>
-        <div className=" flex justify-between my-2 ">
-          <span className=" text-gray-500">Grey Chair</span>
-          <span className=" font-semibold">$19</span>
-        </div>
-        <div className=" flex justify-between">
-          <span className=" text-gray-500">Tooly Table</span>
-          <span className=" font-semibold">$80</span>
-        </div>
+    <div className=" bg-slate-400 dark py-20 px-20 xl:place-content-center grid lg:grid-cols-2 xl:grid-cols-3 gap-10  min-h-screen">
+      <div className=" bg-white dark:bg-black p-6 rounded-3xl shadow-xl flex flex-col justify-between">
+        <span className=" font-semibold dark:text-white text-3xl">
+          Select Item
+        </span>
+        <ul>
+          <div className=" flex justify-between my-2 ">
+            <span className=" text-gray-500 dark:text-gray-100">
+              Grey Chair
+            </span>
+            <span className=" font-semibold dark:text-gray-100">$19</span>
+          </div>
+          <div className=" flex justify-between my-2">
+            <span className=" text-gray-500 dark:text-gray-100">
+              Tooly Table
+            </span>
+            <span className=" font-semibold dark:text-gray-100">$80</span>
+          </div>
+        </ul>
         <div className=" flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className=" font-semibold">$99</span>
         </div>
-        <button className=" mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+        <button className=" mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white p-3 text-center rounded-xl w-3/4 mx-auto dark:hover:bg-white hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
           Checkout
         </button>
       </div>
