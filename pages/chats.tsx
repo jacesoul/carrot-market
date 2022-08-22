d0da88ba7b26119f78995fb098a86f6d72f90d4a
jacesoul <seolju90@gmail.com>
2022-08-22 19:36:03 +0900

    Chats

---
+++ pages/chats.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+
+const Chats: NextPage = () => {
+  return (
+    <div className=" py-10 divide-y-[1px]">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div className=" flex cursor-pointer py-3 items-center space-x-3 px-4 ">
+          <div className=" w-12 h-12 bg-slate-300 rounded-full" />
+          <div>
+            <p className=" text-gray-700">Steve Jobs</p>
+            <p className=" text-sm text-gray-500">
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Chats;
