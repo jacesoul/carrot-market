a8dbcf89e1cf2c1d35b1a3d7d290b47e3b0a84ef
jacesoul <seolju90@gmail.com>
2022-08-25 11:26:02 +0900

    Stream

---
+++ pages/stream/[id].tsx
@@ -0,0 +1,149 @@
+import type { NextPage } from "next";
+
+const StreamDetail: NextPage = () => {
+  return (
+    <div className=" py-10 px-4 space-y-4 ">
+      <div className=" w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+      <h3 className=" text-gray-800 font-semibold text-2xl mt-2">
+        Let's try potatos
+      </h3>
+      <div className=" py-10 px-4 space-y-4 pb-16 h-[50vh] overflow-y-scroll">
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+        <div className=" flex items-start space-x-2">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className=" flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className=" w-8 h-8 rounded-full bg-slate-400" />
+          <div className=" w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want $200</p>
+          </div>
+        </div>
+      </div>
+      <div className=" fixed bottom-2 inset-x-0 py-2">
+        <div className=" flex items-center relative max-w-md w-full mx-auto">
+          <input
+            type="text"
+            className=" shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500 pr-12"
+          />
+          <div className=" absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-400 text-sm text-white">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StreamDetail;
