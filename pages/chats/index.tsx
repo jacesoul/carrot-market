21aab80ebbc103f04354bfcaa8d23ac8e7f99a88
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-08-29 09:34:10 +0900

    Layout part Two

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,20 +1,23 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className=" py-10 divide-y-[1px]">
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div className=" flex cursor-pointer py-3 items-center space-x-3 px-4 ">
-          <div className=" w-12 h-12 bg-slate-300 rounded-full" />
-          <div>
-            <p className=" text-gray-700">Steve Jobs</p>
-            <p className=" text-sm text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title="Chats" hasTabBar>
+      <div className=" py-10 divide-y-[1px]">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div className=" flex cursor-pointer py-3 items-center space-x-3 px-4 ">
+            <div className=" w-12 h-12 bg-slate-300 rounded-full" />
+            <div>
+              <p className=" text-gray-700">Steve Jobs</p>
+              <p className=" text-sm text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
