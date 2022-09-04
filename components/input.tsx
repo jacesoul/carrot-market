11e63ba31411e05ae3f66ebb4ad636fb653ecaca
jacesoul <seolju90@gmail.com>
2022-09-04 20:33:08 +0900

    Implement Input, Button components on Enter.tsx

--- components/input.tsx
+++ components/input.tsx
@@ -44,8 +44,10 @@ export default function Input({
         </div>
       ) : null}
       {kind === "phone" ? (
-        <div>
-          <span></span>
+        <div className=" flex rounded-md shadow-sm">
+          <span className=" flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+            +82
+          </span>
           <input
             id={name}
             {...rest}
