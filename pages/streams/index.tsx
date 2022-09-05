37ae80c96323ebcd319715fb330708ba3f876e2a
jacesoul <seolju90@gmail.com>
2022-09-05 10:01:45 +0900

    Create floating-button components and implement overall pages

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import FloatingButton from "../../components/floating-button";
 import Layout from "../../components/layout";
 
 const Stream: NextPage = () => {
@@ -11,7 +12,7 @@ const Stream: NextPage = () => {
             <h3 className=" text-gray-700 text-lg mt-2">Let's try potatos</h3>
           </div>
         ))}
-        <button className=" fixed hover:bg-orange-500 cursor-pointer transition-colors bottom-20 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl border-transparent">
+        <FloatingButton href="/streams/create">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -25,7 +26,7 @@ const Stream: NextPage = () => {
               d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
             />
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
