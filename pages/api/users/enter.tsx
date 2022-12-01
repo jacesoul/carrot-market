6ab2b8174ee9ea63cab994c36dbaba9c5b68cd8e
jacesoul <seolju90@gmail.com>
2022-12-02 00:03:59 +0900

    Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -24,6 +27,14 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
+      to: process.env.PHONE_NUMBER!, // PHONE_NUMBER라는 환경변수가 존재하지 않을수도 있기 때문에 확실히 존재하는 변수라고 타입스크립트에게 알려준다.
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
